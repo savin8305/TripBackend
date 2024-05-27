6c7cdeda1f99edbd8f62961a6e8a2502869a277a
kanha-9770 <app3.nessco@gmail.com>
2024-05-27 17:49:15 +0530

    hello

--- backend/server.js
+++ backend/server.js
@@ -16,7 +16,7 @@ setupRoutes(app);
 console.log("process.env.MONGO_URI",process.env.MONGO_URI);
 console.log("SPREADSHEET_ID",process.env.SPREADSHEET_ID);
 app.use("/",(req,res)=>{
-  res.send("Hello Word")
+ console.log("Hello Word");
 })
 app.delete("/deletedata/:id", async (req, res) => {
   const id = req.params.id;
